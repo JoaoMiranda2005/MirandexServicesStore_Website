7d4bf91e16a74d18d408fde31bc79a37ac01172e
João Miranda <jprmiranda678@gmail.com>
2024-02-29 14:11:37 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const repoContainer = document.querySelector('.container');
+  
+    fetch('https://api.github.com/users/JoaoMiranda2005/repos')
+      .then(response => response.json())
+      .then(data => {
+        // Sort repositories by creation date in descending order
+        data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+  
+        // Render sorted repositories
+        data.forEach(repo => {
+          const repoItem = document.createElement('div');
+          repoItem.classList.add('repo-item');
+          repoItem.innerHTML = `
+            <h3>${repo.name}</h3>
+            <p>${repo.description || 'No description'}</p>
+            <a href="${repo.html_url}" target="_blank">View Repo</a>
+          `;
+          repoContainer.appendChild(repoItem);
+        });
+      })
+      .catch(error => console.error('Error fetching repositories:', error));
+  });
+  
+
