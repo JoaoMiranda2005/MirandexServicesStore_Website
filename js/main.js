9d505c593b93291fdffbb84fe0ea44a4cb3bad8c
João Miranda <jprmiranda678@gmail.com>
2024-02-29 20:10:01 +0000

    merdas alteradas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 document.addEventListener('DOMContentLoaded', () => {
-    const repoContainer = document.querySelector('.container');
+    const repoContainer = document.querySelector('.containerrepos');
   
     fetch('https://api.github.com/users/JoaoMiranda2005/repos')
       .then(response => response.json())
